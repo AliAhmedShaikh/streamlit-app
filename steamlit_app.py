ae55b84d896256abefff001c950d4cf922d8c68c
AliAhmedShaikh <33371550+AliAhmedShaikh@users.noreply.github.com>
2022-09-04 11:48:31 -0700

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -38,7 +38,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+streamlit.text(streamlit)
 my_cnx = snowflake.connector.connect(**streamlit.secrets)
+
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
