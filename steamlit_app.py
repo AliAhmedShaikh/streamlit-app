04544bc2dca52df550e989bc34e0a0bae1de8ff7
AliAhmedShaikh <33371550+AliAhmedShaikh@users.noreply.github.com>
2022-09-04 11:59:19 -0700

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -37,8 +37,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cnx = snowflake.connector.connect(**streamlit.secrets)
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cnx = snowflake.connector.connect(**streamlit.secrets)
 
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
